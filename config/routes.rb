69347d6d3fa0506a4e84994f9a9a242226f7708a
jimmysanfrancisco <jdefrancesco2@gmail.com>
2018-06-28 15:24:12 -0400

    Wires up page linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+
+  root to: 'home#index'
+
+  get '/about', to: "home#about"
+  get '/work', to: "home#work"
+  get '/contact', to: "home#contact"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
